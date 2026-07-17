576e890301f9b412379ab3a0f816d102105acf78
wadzamurombedzi <wadzamurombedzi@gmail.com>
2026-07-18 00:32:49 +0300

    week 1 done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require(`express`).Router();
 
-//router.get('/', (req, res) => {res.send(`Hello World`);});
+router.get('/', (req, res) => {res.send(`Hello World`);});
 
 router.use('/users', require('./users'));
 
